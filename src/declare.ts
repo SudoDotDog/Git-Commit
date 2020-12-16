9c003dd623475c7876d2152aa5d17b2bd6045236
WMXPY <wm@mengw.io>
2020-12-16 13:52:10 -0600

    add git commit info

--- src/declare.ts
+++ src/declare.ts
@@ -4,6 +4,16 @@
  * @description Declare
  */
 
+export type GitCommitInfo = {
+
+    readonly type: string;
+    readonly modules: string[];
+    readonly subject: string;
+
+    readonly explanation?: string[];
+    readonly resolve?: string;
+};
+
 export type GitCommitPatternType = {
 
     readonly deliver: string;
