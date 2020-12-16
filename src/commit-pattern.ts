5a634f778392b3629db2fb8e1649b2f72a7ae433
WMXPY <wm@mengw.io>
2020-12-16 13:26:53 -0600

    add commit pattern class base

--- src/commit-pattern.ts
+++ src/commit-pattern.ts
@@ -4,14 +4,46 @@
  * @description Commit Pattern
  */
 
+import { GitCommitPatternModule, GitCommitPatternRecord, GitCommitPatternType } from "./declare";
+
 export class GitCommitPattern {
 
-    public static create(): GitCommitPattern {
+    public static empty(): GitCommitPattern {
 
-        return new GitCommitPattern();
+        return new GitCommitPattern({
+            allowWorkInProgress: true,
+            types: [],
+            modules: [],
+        });
     }
 
-    private constructor() {
-        // TODO
+    public static fromRecord(record: GitCommitPatternRecord): GitCommitPattern {
+
+        return new GitCommitPattern(record);
+    }
+
+    private _allowWorkInProgress: boolean;
+
+    private readonly types: Map<string, GitCommitPatternType>;
+    private readonly modules: Map<string, GitCommitPatternModule>;
+
+    private constructor(record: GitCommitPatternRecord) {
+
+        this._allowWorkInProgress = record.allowWorkInProgress;
+
+        this.types = new Map<string, GitCommitPatternType>();
+        this.modules = new Map<string, GitCommitPatternModule>();
+    }
+
+    public enableWorkInProgress(): this {
+
+        this._allowWorkInProgress = true;
+        return this;
+    }
+
+    public disableWorkInProgress(): this {
+
+        this._allowWorkInProgress = false;
+        return this;
     }
 }
