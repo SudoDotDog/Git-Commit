4e06c0d3235ca503e6ede389a6c501d086bafa9b
WMXPY <wm@mengw.io>
2020-12-18 15:48:32 -0600

    add double colon verify function

---
+++ src/verify/double-colon.ts
@@ -0,0 +1,62 @@
+/**
+ * @author WMXPY
+ * @namespace Verify
+ * @description Double Colon
+ */
+
+import { GitCommitPattern } from "../commit-pattern";
+
+export const verifyDoubleColonCommitMessage = (pattern: GitCommitPattern, message: string): boolean => {
+
+    const type: string | undefined = message.match(/[^:(]+/g)[0];
+
+    if (!type
+        || !pattern.verifyType(type)) {
+        return false;
+    }
+
+    const typeRemoved: string = type.substring(type.length);
+
+    statement: if (typeRemoved.substring(0, 1) === ':') {
+
+        if (typeRemoved.substring(1, 2) === ' '
+            && typeRemoved.substring(1, 2) !== ' ') {
+            break statement;
+        }
+        return false;
+    } else if (typeRemoved.substring(0, 1) === '(') {
+
+        const rightIndex: number = typeRemoved.indexOf(')');
+        const innerContent: string = typeRemoved.substring(1, rightIndex);
+        if (innerContent === '*') {
+            break statement;
+        }
+
+        const splited: string[] = innerContent.split(',');
+        if (splited.length === 0) {
+            return false;
+        }
+
+        for (const module of splited) {
+            if (!pattern.verifyModule(module)) {
+                return false;
+            }
+        }
+
+        const modulesRemoved = typeRemoved.substring(rightIndex);
+        if (modulesRemoved.substring(0, 1) === ':') {
+
+            if (typeRemoved.substring(1, 2) === ' '
+                && typeRemoved.substring(1, 2) !== ' ') {
+                break statement;
+            }
+            return false;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    return true;
+};
